118ea373eb9bd6942a088999e01c91e952e8a289
Christian Martins <cmts2010@gmail.com>
2025-10-22 18:25:26 -0300

    fix: use published angular-cli-ghpages version

--- src/app/modules/portifolio/components/knowledge/knowledge.component.ts
+++ src/app/modules/portifolio/components/knowledge/knowledge.component.ts
@@ -11,30 +11,44 @@ import { IKnowledge } from '../../interface/IKnowledge.interface';
 export class KnowledgeComponent {
   public arrayKnowledge = signal<IKnowledge[]>([
     {
+      src: 'assets/icons/knowledge/angular.svg',
+      alt: 'Ícone de conhecimento de Angular',
+      name: 'Angular 18'
+    },
+    {
+      src: 'assets/icons/knowledge/typescript.svg',
+      alt: 'Ícone de conhecimento de TypeScript',
+      name: 'TypeScript'
+    },
+    {
       src: 'assets/icons/knowledge/javascript.svg',
-      alt: 'Ícone de conhecimento de JavaScript'
+      alt: 'Ícone de conhecimento de JavaScript',
+      name: 'JavaScript'
     },
     {
       src: 'assets/icons/knowledge/html5.svg',
-      alt: 'Ícone de conhecimento de HTML5'
+      alt: 'Ícone de conhecimento de HTML5',
+      name: 'HTML Semântico'
     },
     {
       src: 'assets/icons/knowledge/css3.svg',
-      alt: 'Ícone de conhecimento de CSS3'
+      alt: 'Ícone de conhecimento de CSS3',
+      name: 'CSS moderno'
     },
     {
       src: 'assets/icons/knowledge/react.svg',
-      alt: 'Ícone de conhecimento de React'
-    },
-    {
-      src: 'assets/icons/knowledge/angular.svg',
-      alt: 'Ícone de conhecimento de Angular'
+      alt: 'Ícone de conhecimento de React',
+      name: 'React & Next.js'
     },
     {
       src: 'assets/icons/knowledge/nodejs.svg',
-      alt: 'Ícone de conhecimento de NodeJS'
+      alt: 'Ícone de conhecimento de NodeJS',
+      name: 'Node.js'
     },
-
-
-  ])
+    {
+      src: 'assets/icons/knowledge/tailwind.svg',
+      alt: 'Ícone de conhecimento de Tailwind CSS',
+      name: 'Tailwind & Design System'
+    }
+  ]);
 }
